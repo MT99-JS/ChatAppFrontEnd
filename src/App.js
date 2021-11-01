ff39ade6ceb97592be7dc448299fe302b1cb1a0b
unknown <taimoormuhammad009@gmail.com>
2021-11-01 21:24:56 +0500

    chat-app frontendcomplete

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@ import "./App.css";
 import io from "socket.io-client";
 import { useState } from "react";
 import Chat from "./Chat";
-
-// const socket = io.connect("http://localhost:3001");
-
-const socket = io("http://localhost?userName=tom");
+const socket = io.connect("http://localhost:3001");
 
 function App() {
   const [username, setUsername] = useState("");
